06d7f16e1dcdfb1546ce2fdef2da47555250ff2d
Markus <56866880+MakeleleBot@users.noreply.github.com>
2020-07-12 15:01:03 +0300

    minienemy

---
+++ 3DPeliProjekti2020/Assets/Scripts/EnemyHealthbar1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthbar1 : MonoBehaviour
+{
+    public Slider Enemyslider2;
+
+    public void SetEnemyMaxHealth2(int health2)
+    {
+        Enemyslider2.maxValue = health2;
+        Enemyslider2.value = health2;
+    }
+
+    public void SetEnemyHealth2(int health2)
+    {
+        Enemyslider2.value = health2;
+    }
+
+
+}
